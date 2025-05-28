37e89922e900bccb9a5185c3f709c95218157fe8
musicvibess <ldt2225i@gmail.com>
2025-05-28 10:56:07 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function()  {
+var reply = prompt(“Bienvenid@ ¿cuál es tu nombre?” , “”)
+alert ( “Es un gusto tenerte aquí “ + reply + “!”)
+}
+let miBoton = document.querySelector(‘button’);
+miBoton.onclick = function() {
+   alert(‘Con el código: JT05 obtendrás un 5% en tu próxima compra’);
+}
